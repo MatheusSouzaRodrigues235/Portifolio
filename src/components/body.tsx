88b23abf32b12f66ecdfab630d58819165a914f7
MatheusSouzaRodrigues235 <matheusrodriguesdev671@gmail.com>
2026-05-03 23:36:31 -0300

    :sparkles: Adiciona componente body ao layout principal do aplicativo integra a seção body na estrutura do aplicativo, preparando-se para conteúdo ou recursos adicionais na interface principal. Melhora a consistência do layout (Dados de teste!).

---
+++ src/components/body.tsx
@@ -0,0 +1,20 @@
+const Body : React.FC = () => {
+    return(
+        <div className="bg-[#13131F] mg-auto py-16 px-6 text-white border-b-4 border-[#9B21E5] ">
+            <h1 id="sobre" className="text-7xl font-bold mb-4 text-center" >Sobre Mim</h1>
+            <p className="text-8x1 font-bold mb-4 text-center  ">
+                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its 
+                layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 
+                'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors 
+                now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in 
+                their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose 
+                (injected humour and the like).
+
+            </p>
+        </div>
+
+    );
+
+};
+
+export default Body;    
