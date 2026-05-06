50cb73d9d50a29710d33da8640980a06f0be0d8f
MatheusSouzaRodrigues235 <matheusrodriguesdev671@gmail.com>
2026-05-06 17:04:27 -0300

    :recycle: refactor: Adiciona borda ao grid de tecnologias e remove código comentado

--- src/components/tecnologias.tsx
+++ src/components/tecnologias.tsx
@@ -13,7 +13,7 @@ const Tecnologia: React.FC = () => {
         Tecnologias
       </h1>
 
-      <div className="grid grid-cols-4 gap-4">
+      <div className="grid grid-cols-4 gap-4 border-b-4 border-[#9B21E5]">
         <div>
           <img
             className="h-auto max-w-full"
@@ -47,81 +47,7 @@ const Tecnologia: React.FC = () => {
         </div>
       </div>
 
-      {/* 
-    <div className="bg-[#13131F] text-white p-4 h-full w-full border-b-4 border-[#9B21E5] ">
-        <h1 id="sobre" className="text-7xl font-bold mb-4 text-center mg-auto py-16 px-6" >Tecnologias </h1>    
     
-
-
-        <div className ="m-8 p-8 grid bg-[#13131F] grid-rows-2 
-                     grid-flow-col gap-4 auto-cols-fr"> 
-            <div className = "p-4 bg-[#13131F]">
-                <img className="h-auto max-w-full" src={icones} alt="image description" />
-            </div> 
-            <div className = "p-4 bg-[#13131F]">
-                <img className="h-auto max-w-full" src={icone} alt="image description" />
-            </div> 
-            <div className = "p-4 bg-[#13131F]">
-                <img className="h-auto max-w-full" src={java} alt="image description" />
-            </div> 
-            <div className = "p-4 bg-[#13131F]">
-                <img className="h-auto max-w-full" src={php} alt="image description" />
-            </div> 
-
-             <div className = "p-4 bg-[#13131F]">
-                <img className="h-auto max-w-full" src={icones} alt="image description" />
-            </div> 
-            <div className = "p-4 bg-[#13131F]">
-                <img className="h-auto max-w-full" src={icone} alt="image description" />
-            </div> 
-            <div className = "p-4 bg-[#13131F]">
-                <img className="h-auto max-w-full" src={java} alt="image description" />
-            </div> 
-            <div className = "p-4 bg-[#13131F]">
-                <img className="h-auto max-w-full" src={php} alt="image description" />
-            </div> 
-
-            <div className = "p-4 bg-[#13131F]">
-                <img className="h-auto max-w-full" src={icones} alt="image description" />
-            </div> 
-            <div className = "p-4 bg-[#13131F]">
-                <img className="h-auto max-w-full" src={icone} alt="image description" />
-            </div> 
-            <div className = "p-4 bg-[#13131F]">
-                <img className="h-auto max-w-full" src={java} alt="image description" />
-            </div> 
-            <div className = "p-4 bg-[#13131F]">
-                <img className="h-auto max-w-full" src={php} alt="image description" />
-            </div> 
-            
-            <div className = "p-4 bg-[#13131F]">
-                <img className="h-auto max-w-full" src={icones} alt="image description" />
-            </div> 
-            <div className = "p-4 bg-[#13131F]">
-                <img className="h-auto max-w-full" src={icone} alt="image description" />
-            </div> 
-            <div className = "p-4 bg-[#13131F]">
-                <img className="h-auto max-w-full" src={java} alt="image description" />
-            </div> 
-            <div className = "p-4 bg-[#13131F]">
-                <img className="h-auto max-w-full" src={php} alt="image description" />
-            </div> 
-
-            <div className = "p-4 bg-[#13131F]">
-                <img className="h-auto max-w-full" src={icones} alt="image description" />
-            </div> 
-            <div className = "p-4 bg-[#13131F]">
-                <img className="h-auto max-w-full" src={icone} alt="image description" />
-            </div> 
-            <div className = "p-4 bg-[#13131F]">
-                <img className="h-auto max-w-full" src={java} alt="image description" />
-            </div> 
-            <div className = "p-4 bg-[#13131F]">
-                <img className="h-auto max-w-full" src={php} alt="image description" />
-            </div> 
-        </div> 
-    
-    </div> */}
     </>
   );
 };
