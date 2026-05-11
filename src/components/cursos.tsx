fc962edfad704eafb63384965f38dfaa459b886d
MatheusSouzaRodrigues235 <matheusrodriguesdev671@gmail.com>
2026-05-11 18:14:39 -0300

    :recycle: refactor: Adiciona borda ao contêiner principal da seção Cursos para melhor destaque

--- src/components/cursos.tsx
+++ src/components/cursos.tsx
@@ -13,7 +13,7 @@ const Cursos: React.FC = () => {
     <>
       <h1 className="text-center text-4xl font-bold ">Cursos Complementares</h1>
 
-      <div id="main" className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 m-10">
+      <div id="main" className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 m-10 border-b-4 border-[#9B21E5]">
 
         <div className="bg-[#13131F] rounded-lg m-4  place-items-center border-b-4 border-[#9B21E5] ">
           <div className=" grid grid-rows  ">
