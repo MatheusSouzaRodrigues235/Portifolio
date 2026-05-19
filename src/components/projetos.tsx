da299a0c3b4151798068bf0fd48f69d4f89b2f42
MatheusSouzaRodrigues235 <matheusrodriguesdev671@gmail.com>
2026-05-19 17:24:56 -0300

    :heavy_plus_sign: feat: Adiciona componente Projetos à aplicação

---
+++ src/components/projetos.tsx
@@ -0,0 +1,16 @@
+const Projetos: React.FC = () => {
+  return (
+    <>
+      <div className="bg-[#13131F] rounded-lg content-center  border-b-4 border-[#9B21E5] " >
+        <h1
+          id="sobre"
+          className=" text-5xl font-bold mb-4 text-center mg-auto py-16 px-6"
+        >
+          PROJETOS
+        </h1>
+
+      </div>
+    </>
+  );
+};
+export default Projetos;
