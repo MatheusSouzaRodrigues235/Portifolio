1291eca75308bb99dc88b4e3a2a33107b9d24de5
MatheusSouzaRodrigues235 <matheusrodriguesdev671@gmail.com>
2026-06-15 22:17:11 -0300

    :heavy_plus_sign: feat: Adiciona novos projetos com links e imagens, e implementa função para renderizar modal de detalhes no componente Projetos

--- src/Data/projetoData.ts
+++ src/Data/projetoData.ts
@@ -1,6 +1,7 @@
 import Banner from "../assets/images/Banner.png";
 import herosGames from "../assets/images/herosGames.png";
 import JavaSpringBoot from "../assets/images/JavaSpring.jfif";
+import tccImage from "../assets/images//Images.jpeg";
 // Importação Icones (Talvez seja necessário outra forma de importação)
 import htmlIcon from "../assets/images/icons/html-5.png";
 import cssIcon from "../assets/images/icons/css-3.png";
@@ -13,16 +14,18 @@ import handleBarIcon from "../assets/images/icons/handlebar.svg";
 import mySQL from "../assets/images/icons/mySQL.svg";
 import netBeansIcon from "../assets/images/icons/devicon--netbeans.svg";
 import springBootIcon from "../assets/images/icons/simple-icons--springboot.svg";
-
+import phpIcon from "../assets/images/icons/php.png";
+import tailwindIcon from "../assets/images/icons/Tailwind CSS.png";
 export { ListaProjetos };
 
 // Interface para os dados dos projetos
-interface ProjetosData {
+export interface ProjetosData {
   id: number;
   title: string;
   description: string;
   image?: string;
   tecnologias?: string[];
+  link?: string;
 }
 
 
@@ -32,6 +35,7 @@ const ListaProjetos: ProjetosData[] = [
     id: 1,
     title: "Agendou Tosou",
     description: "Descrição do projeto 1",
+    link: "https://github.com/MatheusSouzaRodrigues235/ProjetoIntegrado2Node",
     image: Banner,
     tecnologias: [htmlIcon, cssIcon, jsIcon, nodeIcon, handleBarIcon, gitIcon],
   },
@@ -39,6 +43,7 @@ const ListaProjetos: ProjetosData[] = [
     id: 2,
     title: "Heros Games",
     description: "Descrição do projeto 2",
+    link:"https://github.com/MatheusSouzaRodrigues235/Projeto-Integrado-Ads-1Trimestre",
     image: herosGames,
     tecnologias:[javaIcon, trelloIcon, mySQL , netBeansIcon, gitIcon],
   },
@@ -46,8 +51,17 @@ const ListaProjetos: ProjetosData[] = [
     id: 3,
     title: "Api com Java Spring Boot",
     description: "Descrição do projeto 2",
+    link:"https://github.com/MatheusSouzaRodrigues235/Trabalho-Uninter-Api-Rest-Spring-Boot",
     image: JavaSpringBoot,
     tecnologias:[javaIcon,  mySQL , springBootIcon, gitIcon],
   },
+  {
+    id: 4, 
+    title: "E-commerce de veículos (TCC ETEC)",
+    description:"E-commerce de veículos desenvolvido como trabalho de conclusão de curso.",
+    link:"https://github.com/MatheusSouzaRodrigues235/E-commerce-Veiculos",
+    image: tccImage,
+    tecnologias:[phpIcon, htmlIcon, cssIcon, mySQL, gitIcon, trelloIcon,tailwindIcon],
+  }
  
 ];
