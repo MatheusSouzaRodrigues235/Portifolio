94720dc103e2dfbe570b93acb0c28da365e29f10
MatheusSouzaRodrigues235 <matheusrodriguesdev671@gmail.com>
2026-06-18 15:05:24 -0300

    :recycle: refactor: Atualiza o componente Body, corrigindo a estrutura do layout, ajustando tamanhos de fonte e escrevendo o texto Sobre Mim

--- src/components/body.tsx
+++ src/components/body.tsx
@@ -1,19 +1,23 @@
-const Body : React.FC = () => {
-    return(
-        <div className="bg-[#13131F] mg-auto py-16 px-6 text-white border-b-4 border-[#9B21E5] pt-50 pb-50 ">
-            <h1 id="sobre" className="text-7xl font-bold mb-4 text-center" >Sobre Mim</h1>
-            <p className="text-8x1 font-bold mb-4 text-center  ">
-                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its 
-                layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 
-                'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors 
-                now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in 
-                their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose 
-                (injected humour and the like).
-
-            </p>        
-        </div>
-    );
-
+const Body: React.FC = () => {
+  return (
+    <div className="bg-[#13131F] mx-auto py-16 px-6 text-white border-b-4 border-[#9B21E5] pt-20 pb-20">
+      <h1 id="sobre" className="text-5xl font-bold mb-6 text-center">
+        Sobre Mim
+      </h1>
+      <p className="text-lg text-gray-300 text-center max-w-4xl mx-auto leading-relaxed">
+        Olá! Sou o Matheus, estudante de Bacharelado em Engenharia de Software.
+        Minha jornada na tecnologia começou cedo, na época em que jogava no meu
+        PS2. Foi ali que descobri minha paixão por videogames e,
+        consequentemente, pelo universo do desenvolvimento de software. Busco
+        sempre evoluir tecnicamente e, além dos estudos acadêmicos na Uninter,
+        dedico meu tempo a cursos complementares , também atuo na equipe de
+        transmissão da Paróquia Coração de Jesus. Sou movido por desafios, pelo
+        aprendizado contínuo tanto na tecnologia quanto em outras áreas de meu
+        interesse e pela oportunidade de colaborar em projetos que gerem impacto
+        positivo.
+      </p>
+    </div>
+  );
 };
 
-export default Body;    
+export default Body;
