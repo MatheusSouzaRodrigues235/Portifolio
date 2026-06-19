cc2f946948d948f9889da7f514aa822e3007a72c
MatheusSouzaRodrigues235 <matheusrodriguesdev671@gmail.com>
2026-06-18 23:05:47 -0300

    :recycle: refactor: Atualiza a configuração do Vite para definir a base dinamicamente com base no comando

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/Portifolio/',
+export default defineConfig(({ command }) => ({
+  base: command === 'serve' ? '/' : '/Portifolio/',
   plugins: [
     react(),
     tailwindcss(),
   ],
-})
+}))
